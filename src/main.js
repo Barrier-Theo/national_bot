66f041d6c765501de82724d84f77b944965bcf3e
Théo Barrier <theo.barrier@hotmail.fr>
2020-08-09 01:12:36 +0200

    display ranking

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const tokenFile = require('../config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const table_scraper = require('table-scraper');
+
+const URL = 'http://www.foot-national.com/partage.php?type=2&id=1';
+
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'nat') {
+        table_scraper.get(URL).then(
+               function (datas) {
+                   let classement = '```';
+                   let array1D = [].concat(...datas);
+                   array1D.shift();
+
+                    array1D.forEach(function(object, index, array){
+                        classement  += object['0'] + '\t' + object['1'] + '\t ' + object['2'] + '\t' + object['3'] + '\t' + object['4'] + '\t' + object['5'] + '\n';
+                   });
+
+                    classement += '```';
+                    msg.channel.send(classement);
+
+               }
+           );
+
+    }
+});
+
+client.login(tokenFile.token);
