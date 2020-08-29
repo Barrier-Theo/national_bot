f84e4f48f3874f223891451549e36f05dcb8af0a
Théo Barrier <theo.barrier@hotmail.fr>
2020-08-29 21:00:28 +0200

    added embed

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-const tokenFile = require('../config.json');
-const Discord = require('discord.js');
+const tokenFile = require("../config.json");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-const table_scraper = require('table-scraper');
+const table_scraper = require("table-scraper");
+const utf8 = require('utf8');
+const iconv = require('iconv');
 
-const URL_CLASSEMENT = 'http://www.foot-national.com/partage.php?type=2&id=1';
+const URL_CLASSEMENT =  "http://www.foot-national.com/partage.php?type=2&id=1 ";
 
 
-client.once('ready', () => {
+client.once( "ready ", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'nat') {
+client.on( "message", msg => {
+    if (msg.content ===  "nat") {
         table_scraper.get(URL_CLASSEMENT).then(
                function (datas) {
-                   let classement = '```';
-                   let datasFormat = ([].concat(...datas)).shift();
-
+                   let datasFormat = ([].concat(...datas));
+                   datasFormat.shift();
+                   const embed = new Discord.MessageEmbed();
+                   embed.setColor('#0765b0')
                    datasFormat.forEach(function(object, index, array){
-                        classement  += object['0'] + '\t' + object['1'] + '\t ' + object['2'] + '\t' + object['3'] + '\t' + object['4'] + '\t' + object['5'] + '\n';
+                       embed.addField(object[0], object[1] + " " + object[2] + " pts")
                    });
-                   classement += '```';
-                   msg.channel.send(classement);
+                   msg.channel.send(embed);
                }
         );
     }
