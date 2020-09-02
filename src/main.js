db4088f9e7bf87d618d47e7c553b82d2d02080e1
LENEVEU Damien <dleneveu@jouve.com>
2020-09-02 16:23:25 +0200

    oui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ const tokenFile = require("../config.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const table_scraper = require("table-scraper");
+const { exec } = require("child_process");
 
-const URL_CLASSEMENT =  "http://www.foot-national.com/partage.php?type=2&id=1 ";
+const URL_CLASSEMENT =  "https://www.foot-national.com/partage.php?type=2&id=1 ";
 
+var convertapi = require('convertapi')(tokenFile.convertapi);
 
 client.once( "ready ", () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,14 +14,27 @@ client.once( "ready ", () => {
 
 client.on( "message", msg => {
     if (msg.content ===  "nat") {
+    exec("webkit2png https://www.foot-national.com/partage.php?type=2&id=1", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+    console.log(`stdout: ${stdout}`);
+    });
         table_scraper.get(URL_CLASSEMENT).then(
                function (datas) {
                    let datasFormat = ([].concat(...datas));
                    datasFormat.shift();
                    const embed = new Discord.MessageEmbed();
+                   embed.attachFiles(['../src/wwwfootnationalcompartagephptype2-full.png'])
+                   embed.setImage('attachment://wwwfootnationalcompartagephptype2-full.png')
                    embed.setColor('#0765b0')
                    datasFormat.forEach(function(object, index, array){
-                       embed.addField(object[0], object[1] + " " + object[2] + " pts")
+                      // embed.addField(object[0], object[1] + " " + object[2] + " pts")
                    });
                    msg.channel.send(embed);
                }
